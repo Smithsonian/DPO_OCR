218facbdaf1fb78b534e23b88fdc7bb12439084d
Luis J. Villanueva <villanueval@si.edu>
2020-11-23 17:31:48 -0500

    Updated to avoid trying to draw boxes outside the image bounds

--- google_vision/run_gvision_ocr.py
+++ google_vision/run_gvision_ocr.py
@@ -5,8 +5,6 @@
 # To run: python3 ocr_to_db.py [path to jpg files]
 #
 
-
-
 import io, json, sys, os, psycopg2, glob
 from PIL import Image, ImageDraw 
 from PIL import ImagePath
@@ -37,6 +35,7 @@ except:
     msg_text = "{subtitle}\n\n{repo}\n{lic}\n\nver. {ver}"
     cur_ver = ver
 
+    
 
 
 f = Figlet(font='slant')
@@ -107,7 +106,6 @@ for filename in list_of_files:
     print("Waiting for API response...")
     response = client.document_text_detection(image=image, image_context=image_context)
 
-
     if os.path.exists('response') == False:
         os.mkdir('response')
 
@@ -117,13 +115,11 @@ for filename in list_of_files:
     ocr_text = response.full_text_annotation.text.split("\n")
 
     print('Full Text: \n=============\n{}\n=============\n'.format(response.full_text_annotation.text))
-
     if os.path.exists('fulltext') == False:
         os.mkdir('fulltext')
 
     with open('fulltext/{}.txt'.format(Path(filename).stem), 'w') as out:
         out.write(response.full_text_annotation.text)
-
 
     #word, confidence, coords
     if os.path.exists('csv') == False:
@@ -135,14 +131,11 @@ for filename in list_of_files:
         os.mkdir('images_annotated')
     img_file = 'images_annotated/{}.jpg'.format(Path(filename).stem)
 
-
     wordfile = open(data_file, "w")
     wordfile.write("word_text,block,page,word,word_line,confidence,vertices_x_0,vertices_y_0,vertices_x_1,vertices_y_1,vertices_x_2,vertices_y_2,vertices_x_3,vertices_y_3\n")
 
-
     #image for PIL
     im = Image.open(filename)
-
 
     word_list = []
     p = 0
@@ -179,16 +172,49 @@ for filename in list_of_files:
             wrd_vertices_x_3 = wrd_vertices[0][0].replace('x: ', '')
             wrd_vertices_y_3 = wrd_vertices[0][1].replace('y: ', '')
             if settings.box_draw == "blocks":
+                if wrd_vertices_x_0 == "" or wrd_vertices_y_0 == "" or wrd_vertices_x_1 == "" or wrd_vertices_y_1 == "" or wrd_vertices_x_2 == "" or wrd_vertices_y_2 == "" or wrd_vertices_x_3 == "" or wrd_vertices_y_3 == "":
+                    continue
                 draw = ImageDraw.Draw(im)
-                draw.line([(int(wrd_vertices_x_0) - settings.line_width, int(wrd_vertices_y_0) - settings.line_width), (int(wrd_vertices_x_1) + settings.line_width, int(wrd_vertices_y_1) - settings.line_width)], fill = linecolor, width = settings.line_width)
-                draw.line([(int(wrd_vertices_x_1) + settings.line_width, int(wrd_vertices_y_1) - settings.line_width), (int(wrd_vertices_x_2) + settings.line_width, int(wrd_vertices_y_2) + settings.line_width)], fill = linecolor, width = settings.line_width)
-                draw.line([(int(wrd_vertices_x_2) + settings.line_width, int(wrd_vertices_y_2) + settings.line_width), (int(wrd_vertices_x_3) - settings.line_width, int(wrd_vertices_y_3) + settings.line_width)], fill = linecolor, width = settings.line_width)
-                draw.line([(int(wrd_vertices_x_3) - settings.line_width, int(wrd_vertices_y_3) + settings.line_width), (int(wrd_vertices_x_0) - settings.line_width, int(wrd_vertices_y_0) - settings.line_width)], fill = linecolor, width = settings.line_width)
+                if int(wrd_vertices_y_0) - settings.line_width < 0:
+                    wrd_vertices_y_0_line = 0
+                else:
+                    wrd_vertices_y_0_line = int(wrd_vertices_y_0) - settings.line_width
+                if int(wrd_vertices_x_1) + settings.line_width > im.size[0]:
+                    wrd_vertices_x_1_line = im.size[0]
+                else:
+                    wrd_vertices_x_1_line = int(wrd_vertices_x_1) + settings.line_width
+                if int(wrd_vertices_y_1) - settings.line_width < 0:
+                    wrd_vertices_y_1_line = 0
+                else:
+                    wrd_vertices_y_1_line = int(wrd_vertices_y_1) - settings.line_width
+                if int(wrd_vertices_x_2) + settings.line_width > im.size[0]:
+                    wrd_vertices_x_2_line = im.size[0]
+                else:
+                    wrd_vertices_x_2_line = int(wrd_vertices_x_2) + settings.line_width
+                if int(wrd_vertices_y_2) + settings.line_width > im.size[1]:
+                    wrd_vertices_y_2_line = im.size[1]
+                else:
+                    wrd_vertices_y_2_line = int(wrd_vertices_y_2) + settings.line_width
+                if int(wrd_vertices_x_3) - settings.line_width < 0:
+                    wrd_vertices_x_3_line = 0
+                else:
+                    wrd_vertices_x_3_line = int(wrd_vertices_x_3) - settings.line_width
+                if int(wrd_vertices_y_3) + settings.line_width > im.size[1]:
+                    wrd_vertices_y_3_line = im.size[1]
+                else:
+                    wrd_vertices_y_3_line = int(wrd_vertices_y_3) + settings.line_width
+                if int(wrd_vertices_x_0) - settings.line_width < 0:
+                    wrd_vertices_x_0_line = 0
+                else:
+                    wrd_vertices_x_0_line = int(wrd_vertices_x_0) - settings.line_width
+                draw.line([(wrd_vertices_x_0_line, wrd_vertices_y_0_line), (wrd_vertices_x_1_line, wrd_vertices_y_1_line)], fill = linecolor, width = settings.line_width)
+                draw.line([(wrd_vertices_x_1_line, wrd_vertices_y_1_line), (wrd_vertices_x_2_line, wrd_vertices_y_2_line)], fill = linecolor, width = settings.line_width)
+                draw.line([(wrd_vertices_x_2_line, wrd_vertices_y_2_line), (wrd_vertices_x_3_line, wrd_vertices_y_3_line)], fill = linecolor, width = settings.line_width)
+                draw.line([(wrd_vertices_x_3_line, wrd_vertices_y_3_line), (wrd_vertices_x_0_line, wrd_vertices_y_0_line)], fill = linecolor, width = settings.line_width)
                 del draw
             db_cursor.execute("INSERT INTO ocr_blocks (document_id, block, confidence, vertices_x_0, vertices_y_0, vertices_x_1, vertices_y_1, vertices_x_2, vertices_y_2, vertices_x_3, vertices_y_3) VALUES (%(document_id)s,%(block)s, %(confidence)s, %(vertices_x_0)s, %(vertices_y_0)s, %(vertices_x_1)s, %(vertices_y_1)s, %(vertices_x_2)s, %(vertices_y_2)s, %(vertices_x_3)s, %(vertices_y_3)s)", {'document_id': document_id, 'block': b, 'confidence': block.confidence, 'vertices_x_0': wrd_vertices_x_0, 'vertices_y_0': wrd_vertices_y_0, 'vertices_x_1': wrd_vertices_x_1, 'vertices_y_1': wrd_vertices_y_1, 'vertices_x_2': wrd_vertices_x_2, 'vertices_y_2': wrd_vertices_y_2, 'vertices_x_3': wrd_vertices_x_3, 'vertices_y_3': wrd_vertices_y_3})
             for paragraph in block.paragraphs:
-                #print('Paragraph confidence: {}'.format(
-                    #paragraph.confidence))
+                print('Paragraph confidence: {}'.format(paragraph.confidence))
                 p += 1
                 word_line = 0
                 for word in paragraph.words:
@@ -217,10 +243,12 @@ for filename in list_of_files:
                     wrd_vertices = [str(word.bounding_box.vertices[3]).split('\n')]
                     wrd_vertices_x_3 = wrd_vertices[0][0].replace('x: ', '')
                     wrd_vertices_y_3 = wrd_vertices[0][1].replace('y: ', '')
-                    #print([word_text, word.confidence, [[wrd_vertices_x_0, wrd_vertices_y_0], [wrd_vertices_x_1, wrd_vertices_y_1], [wrd_vertices_x_2, wrd_vertices_y_2], [wrd_vertices_x_3, wrd_vertices_y_3]]])
+                    #Missing x vertex:
+                    if wrd_vertices_x_3[0:2] == "y:":
+                        wrd_vertices_y_3 = wrd_vertices[0][0].replace('y: ', '')
+                        wrd_vertices_x_3 = 0
                     #Find which line
                     for i in range(word_line, len(ocr_text)):
-                        #print("{}-{}-{} {}".format(i, word_text, ocr_text[i], word_text in ocr_text[i]))
                         if word_text in ocr_text[i]:
                             word_line = i
                             break
@@ -236,37 +264,79 @@ for filename in list_of_files:
                     elif word.confidence <= 0.7:
                         linecolor = "#ff6666"
                     if settings.box_draw == "words":
+                        if wrd_vertices_x_0 == "" or wrd_vertices_y_0 == "" or wrd_vertices_x_1 == "" or wrd_vertices_y_1 == "" or wrd_vertices_x_2 == "" or wrd_vertices_y_2 == "" or wrd_vertices_x_3 == "" or wrd_vertices_y_3 == "":
+                            continue
                         draw = ImageDraw.Draw(im)
-                        draw.line([(int(wrd_vertices_x_0) - settings.line_width, int(wrd_vertices_y_0) - settings.line_width), (int(wrd_vertices_x_1) + settings.line_width, int(wrd_vertices_y_1) - settings.line_width)], fill = linecolor, width = settings.line_width)
-                        draw.line([(int(wrd_vertices_x_1) + settings.line_width, int(wrd_vertices_y_1) - settings.line_width), (int(wrd_vertices_x_2) + settings.line_width, int(wrd_vertices_y_2) + settings.line_width)], fill = linecolor, width = settings.line_width)
-                        draw.line([(int(wrd_vertices_x_2) + settings.line_width, int(wrd_vertices_y_2) + settings.line_width), (int(wrd_vertices_x_3) - settings.line_width, int(wrd_vertices_y_3) + settings.line_width)], fill = linecolor, width = settings.line_width)
-                        draw.line([(int(wrd_vertices_x_3) - settings.line_width, int(wrd_vertices_y_3) + settings.line_width), (int(wrd_vertices_x_0) - settings.line_width, int(wrd_vertices_y_0) - settings.line_width)], fill = linecolor, width = settings.line_width)
+                        if int(wrd_vertices_y_0) - settings.line_width < 0:
+                            wrd_vertices_y_0_line = 0
+                        else:
+                            wrd_vertices_y_0_line = int(wrd_vertices_y_0) - settings.line_width
+                        if int(wrd_vertices_x_1) + settings.line_width > im.size[0]:
+                            wrd_vertices_x_1_line = im.size[0]
+                        else:
+                            wrd_vertices_x_1_line = int(wrd_vertices_x_1) + settings.line_width
+                        if int(wrd_vertices_y_1) - settings.line_width < 0:
+                            wrd_vertices_y_1_line = 0
+                        else:
+                            wrd_vertices_y_1_line = int(wrd_vertices_y_1) - settings.line_width
+                        if int(wrd_vertices_x_2) + settings.line_width > im.size[0]:
+                            wrd_vertices_x_2_line = im.size[0]
+                        else:
+                            wrd_vertices_x_2_line = int(wrd_vertices_x_2) + settings.line_width
+                        if int(wrd_vertices_y_2) + settings.line_width > im.size[1]:
+                            wrd_vertices_y_2_line = im.size[1]
+                        else:
+                            wrd_vertices_y_2_line = int(wrd_vertices_y_2) + settings.line_width
+                        if int(wrd_vertices_x_3) - settings.line_width < 0:
+                            wrd_vertices_x_3_line = 0
+                        else:
+                            wrd_vertices_x_3_line = int(wrd_vertices_x_3) - settings.line_width
+                        if int(wrd_vertices_y_3) + settings.line_width > im.size[1]:
+                            wrd_vertices_y_3_line = im.size[1]
+                        else:
+                            wrd_vertices_y_3_line = int(wrd_vertices_y_3) + settings.line_width
+                        if int(wrd_vertices_x_0) - settings.line_width < 0:
+                            wrd_vertices_x_0_line = 0
+                        else:
+                            wrd_vertices_x_0_line = int(wrd_vertices_x_0) - settings.line_width
+                        draw.line([(wrd_vertices_x_0_line, wrd_vertices_y_0_line), (wrd_vertices_x_1_line, wrd_vertices_y_1_line)], fill = linecolor, width = settings.line_width)
+                        draw.line([(wrd_vertices_x_1_line, wrd_vertices_y_1_line), (wrd_vertices_x_2_line, wrd_vertices_y_2_line)], fill = linecolor, width = settings.line_width)
+                        draw.line([(wrd_vertices_x_2_line, wrd_vertices_y_2_line), (wrd_vertices_x_3_line, wrd_vertices_y_3_line)], fill = linecolor, width = settings.line_width)
+                        draw.line([(wrd_vertices_x_3_line, wrd_vertices_y_3_line), (wrd_vertices_x_0_line, wrd_vertices_y_0_line)], fill = linecolor, width = settings.line_width)
                         del draw
+                        # draw.line([(int(wrd_vertices_x_0) - settings.line_width, int(wrd_vertices_y_0) - settings.line_width), (int(wrd_vertices_x_1) + settings.line_width, int(wrd_vertices_y_1) - settings.line_width)], fill = linecolor, width = settings.line_width)
+                        # draw.line([(int(wrd_vertices_x_1) + settings.line_width, int(wrd_vertices_y_1) - settings.line_width), (int(wrd_vertices_x_2) + settings.line_width, int(wrd_vertices_y_2) + settings.line_width)], fill = linecolor, width = settings.line_width)
+                        # draw.line([(int(wrd_vertices_x_2) + settings.line_width, int(wrd_vertices_y_2) + settings.line_width), (int(wrd_vertices_x_3) - settings.line_width, int(wrd_vertices_y_3) + settings.line_width)], fill = linecolor, width = settings.line_width)
+                        # draw.line([(int(wrd_vertices_x_3) - settings.line_width, int(wrd_vertices_y_3) + settings.line_width), (int(wrd_vertices_x_0) - settings.line_width, int(wrd_vertices_y_0) - settings.line_width)], fill = linecolor, width = settings.line_width)
+                        # del draw
                     word_list.append([word_text, word.confidence, [[wrd_vertices_x_0, wrd_vertices_y_0], [wrd_vertices_x_1, wrd_vertices_y_1], [wrd_vertices_x_2, wrd_vertices_y_2], [wrd_vertices_x_3, wrd_vertices_y_3]]])
                     db_cursor.execute("INSERT INTO ocr_entries (document_id, word_text, block, page, word, word_line, confidence, vertices_x_0, vertices_y_0, vertices_x_1, vertices_y_1, vertices_x_2, vertices_y_2, vertices_x_3, vertices_y_3) VALUES (%(document_id)s, %(word_text)s, %(block)s, %(page)s, %(word)s, %(word_line)s, %(confidence)s, %(vertices_x_0)s, %(vertices_y_0)s, %(vertices_x_1)s, %(vertices_y_1)s, %(vertices_x_2)s, %(vertices_y_2)s, %(vertices_x_3)s, %(vertices_y_3)s)", {'document_id': document_id, 'word_text': word_text, 'block': b, 'page': p, 'word': w, 'word_line': word_line, 'confidence': word.confidence, 'vertices_x_0': wrd_vertices_x_0, 'vertices_y_0': wrd_vertices_y_0, 'vertices_x_1': wrd_vertices_x_1, 'vertices_y_1': wrd_vertices_y_1, 'vertices_x_2': wrd_vertices_x_2, 'vertices_y_2': wrd_vertices_y_2, 'vertices_x_3': wrd_vertices_x_3, 'vertices_y_3': wrd_vertices_y_3})
 
-    #Crop image
-    results_poly = response.text_annotations[0].bounding_poly
-    results_minx = min(results_poly.vertices[0].x, results_poly.vertices[1].x, results_poly.vertices[2].x, results_poly.vertices[3].x)
-    results_miny = min(results_poly.vertices[0].y, results_poly.vertices[1].y, results_poly.vertices[2].y, results_poly.vertices[3].y)
-    results_maxx = max(results_poly.vertices[0].x, results_poly.vertices[1].x, results_poly.vertices[2].x, results_poly.vertices[3].x)
-    results_maxy = max(results_poly.vertices[0].y, results_poly.vertices[1].y, results_poly.vertices[2].y, results_poly.vertices[3].y)
+    if settings.crop == True:
+        #Crop image
+        results_poly = response.text_annotations[0].bounding_poly
+        results_minx = min(results_poly.vertices[0].x, results_poly.vertices[1].x, results_poly.vertices[2].x, results_poly.vertices[3].x)
+        results_miny = min(results_poly.vertices[0].y, results_poly.vertices[1].y, results_poly.vertices[2].y, results_poly.vertices[3].y)
+        results_maxx = max(results_poly.vertices[0].x, results_poly.vertices[1].x, results_poly.vertices[2].x, results_poly.vertices[3].x)
+        results_maxy = max(results_poly.vertices[0].y, results_poly.vertices[1].y, results_poly.vertices[2].y, results_poly.vertices[3].y)
 
-    if results_minx > settings.crop_buffer:
-        results_minx = results_minx - settings.crop_buffer
-    if results_miny > settings.crop_buffer:
-        results_miny = results_miny - settings.crop_buffer
-    if (results_maxx + settings.crop_buffer) < im.size[0]:
-        results_maxx = results_maxx + settings.crop_buffer
-    if (results_maxy + settings.crop_buffer) < im.size[1]:
-        results_maxy = results_maxy + settings.crop_buffer
+        if results_minx > settings.crop_buffer:
+            results_minx = results_minx - settings.crop_buffer
+        if results_miny > settings.crop_buffer:
+            results_miny = results_miny - settings.crop_buffer
+        if (results_maxx + settings.crop_buffer) < im.size[0]:
+            results_maxx = results_maxx + settings.crop_buffer
+        if (results_maxy + settings.crop_buffer) < im.size[1]:
+            results_maxy = results_maxy + settings.crop_buffer
+        print("Cropping image to ({},{}), ({},{})".format(results_minx, results_miny, results_maxx, results_maxy))
+        im1 = im.crop((results_minx, results_miny, results_maxx, results_maxy))
+        #Save cropped image
+        im1.save(img_file, "JPEG")
+    else:
+        im.save(img_file, "JPEG")
+    
+    wordfile.close()
 
-    print("Cropping image to ({},{}), ({},{})".format(results_minx, results_miny, results_maxx, results_maxy))
-
-    im1 = im.crop((results_minx, results_miny, results_maxx, results_maxy))
-
-    #Save cropped image
-    im1.save(img_file, "JPEG")
 
 
 sys.exit(0)
